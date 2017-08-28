c44d1a6a2024db152b9ee791e33418e609cf6f53
silvoneildr <silvoneildr@gmail.com>
2017-08-27 22:14:58 -0300

    Config heroku env.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
-
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/db_pessoa',{ useMongoClient: true })
+
+//process.env.PROD_MONGODB || 'mongodb://localhost/db_finance'
+//mongodb://<dbuser>:<dbpassword>@ds161860.mlab.com:61860/pessoasdb
+
+module.exports = mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost/db_pessoa',
+    { useMongoClient: true }
+)
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!"
 
