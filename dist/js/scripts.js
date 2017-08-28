21fc7b1f2978e75ac574c4b274d39aa838345ccc
silvoneildr <silvoneildr@gmail.com>
2017-08-28 12:20:27 -0300

    Alter dist

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -35148,7 +35148,7 @@ angular.module('personApp')
  
          // se for edição retorna uma pessoa pelo id
         if ($scope.formEdit) {
-            $http.get(`https://app3etapagt4w.herokuapp.com/api/pessoas/${$routeParams.id}`)
+            $http.get(`http://app3etapagt4w.herokuapp.com/api/pessoas/${$routeParams.id}`)
                 .then(function(res) {
                     $scope.person = res.data
                 }, function(res){
@@ -35202,7 +35202,7 @@ angular.module('personApp')
         // altera uma pessoa
         $scope.updatePessoa= function(){
 
-            const url = `https://localhost:3003app3etapagt4w.herokuapp.com/api/pessoas/${$routeParams.id}`
+            const url = `https://app3etapagt4w.herokuapp.com/api/pessoas/${$routeParams.id}`
 
             $http.put(url, $scope.person).then(function(res) {
                 //console.log('Registro atualizado com sucesso!')          
